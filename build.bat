87bc5df284dbf9b34558b55690581a39358ce924
omerfuyar <denemeyo@gmail.com>
2025-12-18 02:06:33 +0200

    -made prettier

--- build.bat
+++ build.bat
@@ -1,15 +1,46 @@
+@echo off
+
 if not exist build mkdir build
 
-arm-none-eabi-as.exe -mcpu=cortex-m0plus -mthumb src\start.s -o build\start.o
-arm-none-eabi-as.exe -mcpu=cortex-m0plus -mthumb src\main.s  -o build\main.o
+echo [1/6] Assembling start.s...
+arm-none-eabi-as.exe -mcpu=cortex-m0plus -mthumb src\start.s -o build\start.o || goto :error
 
-arm-none-eabi-ld.exe -T linker.ld build/start.o build/main.o -o firmware.elf
+echo [2/6] Assembling main.s...
+arm-none-eabi-as.exe -mcpu=cortex-m0plus -mthumb src\main.s  -o build\main.o || goto :error
 
-arm-none-eabi-readelf.exe -h firmware.elf > build\elfdata.txt
-arm-none-eabi-objdump.exe -d firmware.elf > build\objdata.txt
+echo [3/6] Linking...
+arm-none-eabi-ld.exe -T linker.ld build/start.o build/main.o -o firmware.elf || goto :error
 
-elf2uf2.exe -i firmware.elf -f 0xe48bff56 -o build\firmware.uf2
+echo [4/6] Generating ELF data...
+arm-none-eabi-readelf.exe -h firmware.elf > build\elfdata.txt || goto :error
+arm-none-eabi-objdump.exe -d firmware.elf > build\objdata.txt || goto :error
 
+echo [5/6] Converting to UF2...
+elf2uf2.exe -i firmware.elf -f 0xe48bff56 -o build\firmware.uf2 || goto :error
+
+echo Cleaning up...
 del build\*.o firmware.elf
 
-copy build\firmware.uf2 F:\
+echo ----------------
+echo Build Successful
+echo ----------------
+
+echo [6/6] Copying to device...
+copy build\firmware.uf2 F:\ || goto :errorFlash
+
+echo ----------------
+echo Flash Successful!
+echo ----------------
+exit /b 0
+
+:error
+echo ----------------
+echo Build Failed!
+echo ----------------
+exit /b 1
+
+:errorFlash
+echo ----------------
+echo Flash Failed!
+echo ----------------
+exit /b 2
--- src/main.s
+++ src/main.s
@@ -4,65 +4,80 @@
 .thumb_func
 
 .global main
+    .equ RESETS_BASE, 0x4000c000
+    .equ RESETS_CLEAR, 0x3000
 
-/* ===== RESET REGISTERS ===== */
-.equ RESETS_BASE,     0x4000C000
-.equ RESETS_CLR,      0x3000
-.equ RESETS_DONE,     0x4000C008
+    .equ RESET_IO_BANK0, (1<<5)
+    .equ RESET_PADS, (1<<8)
+    .equ RESET_SIO, (1<<3)
 
-/* Reset bits */
-.equ RESET_IO_BANK0,  (1 << 5)
-.equ RESET_PADS,      (1 << 8)
-.equ RESET_SIO,       (1 << 3)
+    .equ IO_BANK0_BASE, 0x40014000
+    .equ PADS_BANK0_BASE, 0x4001c000
 
-/* ===== GPIO ===== */
-.equ GPIO25_CTRL,     0x400140CC
+    .equ GPIO17_STATUS, 0x088
+    .equ GPIO17_CTRL, 0x08c
+    .equ GPIO17_BIT, (1<<17)
+    .equ GPIO17_PAD, 0x48
 
-/* ===== SIO ===== */
-.equ SIO_BASE,        0xD0000000
-.equ GPIO_OE_SET,     0x20
-.equ GPIO_OUT_SET,    0x14
-.equ GPIO_OUT_CLR,    0x18
+    .equ GPIO18_STATUS, 0x090
+    .equ GPIO18_CTRL, 0x094
+    .equ GPIO18_BIT, (1<<18)
+    .equ GPIO18_PAD, 0x4c
 
-.equ GPIO25_BIT,      (1 << 25)
+    .equ GPIO25_STATUS, 0x0c8
+    .equ GPIO25_CTRL, 0x0cc
+    .equ GPIO25_BIT, (1<<25)
+    .equ GPIO25_PAD, 0x68
+
+    .equ SIO_BASE, 0xD0000000 // single-cycle IO bank
+    .equ GPIO_OE_SET, 0x20 // output enable
+    .equ GPIO_OUT_SET, 0x14
+    .equ GPIO_OUT_CLR, 0x18
+
+    .equ BLINK_VALUE, 0x00100000
 
 .section .text
 .thumb_func
 main:
-    /* --- Release peripheral resets --- */
-    ldr r0, =RESETS_BASE
-    ldr r1, =(RESET_IO_BANK0 | RESET_PADS | RESET_SIO)
-    
-    /* FIX 1: Load offset into register to avoid "value too big" error */
-    ldr r2, =RESETS_CLR
-    str r1, [r0, r2]
+    // initiate reset for IO_BANK0, PADS and SIO
+    ldr r0, =RESETS_BASE // start location of resets
+    ldr r1, =RESETS_CLEAR // offset of clear register
+    ldr r2, =(RESET_IO_BANK0| RESET_PADS| RESET_SIO)// configuration of the reset
+
+    str r2, [r0, r1]// apply the configured reset by writing to offsetted base
 
 reset_wait:
-    /* FIX 2: Correctly wait for ALL bits to be set */
-    ldr r2, [r0, #0x8]            /* Read RESETS_DONE */
-    
-    /* Fix: Use 'ands' because M0+ AND instruction always updates flags */
-    ands r2, r1                   /* Mask out bits we don't care about */
-    
-    cmp r2, r1                    /* Check if result matches our mask exactly */
-    bne reset_wait                /* If not equal, keep waiting */
+    // keep checking if the value in address 'RESETS_BASE + 8' is the configuration we want
+    ldr r1, [r0, #8]
+    ands r1, r2 // masking out the unnecessary bits
+    cmp r1, r2
 
-    /* --- Configure GPIO25 function (SIO) --- */
-    ldr r0, =GPIO25_CTRL
+    bne reset_wait // go back if not match
+
+    // write value '5 (0b101)' to address 'IO_BANK0_BASE + GPIO25_CTRL' to set function to SIO
+    ldr r0, =(IO_BANK0_BASE+ GPIO25_CTRL)
     movs r1, #5
     str r1, [r0]
 
-    /* --- Enable GPIO25 output --- */
+    /*
+    ldr r0, =(IO_BANK0_BASE+ GPIO17_CTRL)
+    movs r1, #5
+    str r1, [r0]
+    // configure pads (input enable)
+    ldr r0, =(PADS_BANK0_BASE+ GPIO18_PAD)
+    movs r1, #0b1000000
+    str r1, [r0]
+    */
+
+    // apply bit mask to the address 'SIO_BASE + GPIO_OE_SET' to configure pins as output (output enable)
     ldr r0, =SIO_BASE
-    ldr r1, =GPIO25_BIT
+    ldr r1, =(GPIO25_BIT| GPIO17_BIT)
     str r1, [r0, #GPIO_OE_SET]
 
 blink_loop:
-    /* LED ON */
-    str r1, [r0, #GPIO_OUT_SET]
+    str r1, [r0, #GPIO_OUT_SET]// write GPIO25_BIT to the address 'SIO_BASE + GPIO_OUT_SET'
     bl delay
 
-    /* LED OFF */
     str r1, [r0, #GPIO_OUT_CLR]
     bl delay
 
@@ -70,7 +85,7 @@ blink_loop:
 
 .thumb_func
 delay:
-    ldr r2, =0x00100000
+    ldr r2, =BLINK_VALUE
 delay_loop:
     subs r2, r2, #1
     bne delay_loop
